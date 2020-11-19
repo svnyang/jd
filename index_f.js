457c40b90b1c6ceae8b0475b978b53ef6a9d51bc
sunyang <38999312+svnyang@users.noreply.github.com>
2020-11-19 21:58:22 +0800

    Update index_f.js

--- index_f.js
+++ index_f.js
@@ -22,8 +22,8 @@ cron "10 0 0 * * *" script-path=https://raw.githubusercontent.com/dompling/Scrip
 10 0 0 * * * https://raw.githubusercontent.com/dompling/Script/master/birthdayCountDown/index.js
 */
 var mediaImg = ""; // 头像图片默认显示第一张图片，随机 bing 应 api 背景：https://api.dujin.org/pic
-var username = ""; // 姓名
-var birthday = ""; // 生日日期
+var username = "父亲"; // 姓名
+var birthday = "1970-03-13"; // 生日日期
 var physiologicalDefault = ""; // 最近一次来周期时间
 var physiologicalCycle = ""; // 下一次周期
 var eday = ""; // 相识日期
